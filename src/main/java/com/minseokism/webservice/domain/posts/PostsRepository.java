8bb1b48fb890a3e7bd4ecc60040314854858ce53
Minseok Lee <lagender@gmail.com>
2018-03-20 14:20:30 +0900

    2-1. 도메인 코드 만들기

---
+++ src/main/java/com/minseokism/webservice/domain/posts/PostsRepository.java
@@ -0,0 +1,5 @@
+package com.minseokism.webservice.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {}
