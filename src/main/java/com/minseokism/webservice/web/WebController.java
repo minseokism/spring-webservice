dd54ab47fb5c14862b2baa81ed1ef077b8702b80
Minseok Lee <lagender@gmail.com>
2018-03-21 15:35:12 +0900

    3-1. Handlebars 연동

---
+++ src/main/java/com/minseokism/webservice/web/WebController.java
@@ -0,0 +1,17 @@
+package com.minseokism.webservice.web;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@AllArgsConstructor
+public class WebController {
+
+    @GetMapping("/")
+    public String main() {
+        return "main";
+    }
+
+}
