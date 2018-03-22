81aa7ba2ed068c850cb765e4b37f9ca2c8b186dc
Minseok Lee <lagender@gmail.com>
2018-03-22 11:27:29 +0900

    3-2. 게시글 등록 - 입력화면

--- src/main/java/com/minseokism/webservice/web/WebRestController.java
+++ src/main/java/com/minseokism/webservice/web/WebRestController.java
@@ -1,6 +1,5 @@
 package com.minseokism.webservice.web;
 
-import com.minseokism.webservice.domain.posts.PostsRepository;
 import com.minseokism.webservice.dto.posts.PostsSaveRequestDto;
 import com.minseokism.webservice.service.PostService;
 import lombok.AllArgsConstructor;
@@ -21,8 +20,8 @@ public class WebRestController {
     }
 
     @PostMapping("/posts")
-    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
-        postService.save(dto);
+    public Long savePosts(@RequestBody PostsSaveRequestDto dto) {
+        return postService.save(dto);
     }
 
 }
